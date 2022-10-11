fc03598380df56540ce1d286871439c4e2d11d21
judatop <judagarciaclavijo@gmail.com>
2022-10-11 12:58:35 -0500

    endpoint ws

--- src/helpers/endpoints.js
+++ src/helpers/endpoints.js
@@ -1,4 +1,4 @@
-export const API_ENDPOINT = "https://9199-45-171-120-3.sa.ngrok.io";
+export const API_ENDPOINT = "https://821c-45-171-120-3.sa.ngrok.io";
 
 export const LOGIN_ENDPOINT = API_ENDPOINT + "/login";
 export const GET_ORDERS_ENDPOINT = API_ENDPOINT + "/orders";
